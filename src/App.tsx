9abad28228e46fe2176ef3b442357f0d837c65d8
Eric Thomas <eric@ericthomas.ca>
2023-07-12 19:03:18 -0230

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+export default function App() {
+
+  return (
+    <>
+      <h1>Welcome to my App!</h1>
+    </>
+  )
+}
