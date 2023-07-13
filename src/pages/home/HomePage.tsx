da13ca1da321976e03bfc1e9987cd4211f2368b1
Eric Thomas <eric@ericthomas.ca>
2023-07-13 19:54:17 -0230

    put buttons on home screen

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,19 +1,44 @@
+import { Button } from "@mui/material";
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
 export default function HomePage() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "75vh",
-      }}
-    >
-      <Typography variant='h1' justifyContent='center' align='center'>
-        Home Page Component
-      </Typography>
-    </Box>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          // height: "75vh",
+        }}
+      >
+        <Typography
+          marginTop={3}
+          variant='h3'
+          justifyContent='center'
+          align='center'
+        >
+          Home
+        </Typography>
+      </Box>
+      <Stack marginTop={4} marginLeft={13} marginRight={13} spacing={4}>
+        <Button variant='contained' href='/books'>
+          Books
+        </Button>
+        <Button variant='contained' href='/characters'>
+          Characters
+        </Button>
+        <Button variant='contained' href='/movies'>
+          Movies
+        </Button>
+        <Button variant='contained' href='/spells'>
+          Spells
+        </Button>
+        <Button variant='contained' href='/potions'>
+          Potions
+        </Button>
+      </Stack>
+    </>
   );
 }
